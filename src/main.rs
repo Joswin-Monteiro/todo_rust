0f0933ef7ca0e892448adbc57bd9d9ca9d020536
Joswin Presten Monteiro <joswinpresten20@gmail.com>
2024-11-18 15:28:50 +0530

    asdfsda

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Not enough arguments");
+    } else {
+        for arg in args.iter() {
+            println!("{}", arg);
+        }
+    }
 }
